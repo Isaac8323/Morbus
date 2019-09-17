21c0524783b12daf6f3068ee15ddceaaaa1957c4
Isaac8323 <55283588+Isaac8323@users.noreply.github.com>
2019-09-17 09:47:32 -0500

    primer commit

---
+++ Assets/Scripts/AdminMYSQL.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MySql.Data.MySqlClient;
+public class AdminMYSQL : MonoBehaviour
+{
+    public string servidorBaseDatos;
+    public string nombreBaseDatos;
+    public string usuarioVaseDatos;
+    public string contraseñaBaseDatos;
+
+    public string datosConexion;
+    public MySqlConnection conexion;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    public MySqlConnection ConectarConServidorBaseDatos()
+    {
+        datosConexion = "Server=" + servidorBaseDatos
+              + ";Database=" + nombreBaseDatos
+              + ";Uid=" + usuarioVaseDatos
+              + ";Pwd=" + ""
+              + ";";
+        conexion = new MySqlConnection(datosConexion);
+        try
+        {
+            conexion.Open();
+            Debug.Log("Conexion con BD correcta");
+        }
+        catch(MySqlException error){
+            Debug.Log("Imposible conectar con BD morbus: " + error);
+        }
+        return conexion;
+    }
+}
