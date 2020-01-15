2de01a8e8becf40b6db049aeba45a2dfefd0051a
Isaac8323 <55283588+Isaac8323@users.noreply.github.com>
2020-01-15 04:26:45 -0600

    BattleAnimation
    Puras tristezas y decepciones...

---
+++ Assets/Scripts/Organismo/Actions.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Actions : MonoBehaviour
+{
+    public GameObject character;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void attackone()
+    {
+        character.GetComponent<Animator>().Play("test01_atck1");
+    }
+    public void attacktwo()
+    {
+        character.GetComponent<Animator>().Play("test01_atck2");
+    }
+    public void attackthree()
+    {
+        character.GetComponent<Animator>().Play("test01_atck3");
+    }
+}
