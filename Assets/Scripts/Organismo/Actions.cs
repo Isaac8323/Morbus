d0c09561190f7db8e312818442a04100f93640da
Isaac8323 <55283588+Isaac8323@users.noreply.github.com>
2020-04-06 05:03:19 -0500

    CambiosTuto
    ola

--- Assets/Scripts/Organismo/Actions.cs
+++ Assets/Scripts/Organismo/Actions.cs
@@ -5,135 +5,44 @@ using UnityEngine.UI;
 
 public class Actions : MonoBehaviour
 {
-    public bool isatckin;
-    public float delay;
-    int flagdelay;
-    public int turnoj;
-    public int turnoa;
-    public GameObject character, boss, atck01, atck02, atck03, atck04, damage, damaged, music, vic;
-    public int BossLife;
-    public int AspirinaLife;
-    Archivos healthpoints;
-    Text HPC, HPB, status;
+    public Vector3 pos;
+    public Vector3 dest;
+    public Transform mov;
+
     // Start is called before the first frame update
     void Start()
     {
-        damage.SetActive(false);
-        damaged.SetActive(false);
-        Deactivate();
-        turnoj = 1;        
-        healthpoints = GameObject.Find("Canvas").GetComponent<Archivos>();
-        AspirinaLife = 125000;
-        BossLife = 25000;
-        HPC = GameObject.Find("Aspirina_life").GetComponentInChildren<Text>();
-        HPC.text = AspirinaLife.ToString();
-        HPB = GameObject.Find("Cefalea_life").GetComponentInChildren<Text>();
-        HPB.text = BossLife.ToString();
-        status = GameObject.Find("Status").GetComponentInChildren<Text>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (BossLife > 0)
-        {
-            if (turnoj == 1)
-            {
-                Deactivate();
-                delay += Time.deltaTime;
-                if (delay > 3)
-                {
-                    status.text = "Cefalea utilizo: Contagion";
-                    AspirinaLife = AspirinaLife - 8333;
-                    turnoj = 0;
-                    delay = delay - delay;
-                }
-            }
-        }
-        if(BossLife <=0)
-        {
-            boss.SetActive(false);
-            BossLife = 0;
-            Deactivate();
-            status.text = "Victoria!";
-        }
-        if(turnoj == 0)
-        {
-            delay += Time.deltaTime;
-            if(delay > 3)
-            {
-                status.text = "Turno: Aspirina";
-                if(turnoa == 0)
-                {
-                    atck01.SetActive(true);
-                }
-                if(turnoa == 1)
-                {
-                    atck01.SetActive(true);
-                    atck02.SetActive(true);
-                }
-                if(turnoa == 2)
-                {
-                    atck01.SetActive(true);
-                    atck02.SetActive(true);
-                    atck03.SetActive(true);
-                }
-                if(turnoa == 3)
-                {
-                    atck01.SetActive(true);
-                    atck02.SetActive(true);
-                    atck03.SetActive(true);
-                    atck04.SetActive(true);
-                }
-            }
-        }
 
-        HPC.text = AspirinaLife.ToString();
-        HPB.text = BossLife.ToString();
     }
 
-    public void Deactivate()
+    public void Arrow(string bot)
     {
-        atck01.SetActive(false);
-        atck02.SetActive(false);
-        atck03.SetActive(false);
-        atck04.SetActive(false);
+        mov = GameObject.Find(bot).GetComponent<Transform>();
+        pos = mov.transform.position;
+        dest = new Vector3(152, pos.y + 4, pos.z);
+        GameObject.Find("Aspirina_arrow01").transform.position = dest;
     }
 
-    IEnumerator Attack(int damage)
+    public void HOne()
     {
-        yield return new WaitForSeconds(2);        
-        character.GetComponent<Animator>().Play("Stand");
-        if (turnoa < 4)
-        {
-            turnoa++;
-        }
-        if (turnoa > 3)
-        {
-            turnoa = 0;
-        }
-        BossLife = BossLife - damage;
-        turnoj = 1;
+        Arrow("Aspirina_Hab_01");
     }
 
-    public void attackone()
+    public void HTwo()
     {
-        character.GetComponent<Animator>().Play("Aspirina_atack01");
-        StartCoroutine(Attack(8350));
+        Arrow("Aspirina_Hab_02");
     }
-    public void attacktwo()
+    public void HThree()
     {
-        character.GetComponent<Animator>().Play("Aspirina_atack02");
-        StartCoroutine(Attack(8350));
+        Arrow("Aspirina_Hab_03");
     }
-    public void attackthree()
+    public void HFour()
     {
-        character.GetComponent<Animator>().Play("Aspirina_atack03");
-        StartCoroutine(Attack(8350));
-    }
-    public void attackfour()
-    {
-        character.GetComponent<Animator>().Play("Aspirina_atack04");
-        StartCoroutine(Attack(8350));
+        Arrow("Aspirina_Hab_04");
     }
 }
